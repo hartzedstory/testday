8d7f9c7984d1dbf06fc74ae719da118cb709de28
hartzedstory <92392395+hartzedstory@users.noreply.github.com>
2022-04-02 20:26:09 +0700

    Add files via upload

---
+++ KiemTra-BAI5/KiemTra-BAI5/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KiemTra-BAI5
+//
+//  Created by Hartzed Story on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buttonField: UIView!
+    @IBOutlet weak var passwordField: UIView!
+    @IBOutlet weak var userNameField: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passwordField.layer.cornerRadius = 20
+        passwordField.layer.cornerRadius = 20
+        buttonField.layer.cornerRadius = 20
+    }
+
+
+}
+
---
+++ KiemTra/KiemTra/main.swift
@@ -0,0 +1,62 @@
+import Foundation
+print("Bai Kiem Tra")
+
+//MARK: BAI 1: DOAN SO RANDOM
+//print("Chuong trinh do vui co thuong")
+//var randomNumber = Int.random(in: 1...50)
+//var soNhap: Int
+//repeat {
+//    print("Nhap so may man cua ban")
+//    soNhap = Int(readLine() ?? "") ?? 0
+//    if randomNumber == soNhap {
+//        print("Chuc mung ban da la nguoi may man")
+//        }
+//    if randomNumber != soNhap {
+//        print("Chuc ban may man lan sau!")
+//        }
+//} while soNhap < 1 || soNhap > 50
+//        print("Vui long nhap lai")
+//MARK: BAI 2: TINH CUOC TAXI
+//print("Chuong trinh tinh cuoc taxi")
+//print("Nhap so kilomet ban da di")
+//var soKM = Float(readLine() ?? "") ?? 0.0
+//var soTien: Float
+//if soKM >= 1 && soKM <= 29
+//{
+//    soTien = soKM * 9500.0 + 11000.0
+//    print("So tien phai tra la: \(soTien)")
+//}
+//if soKM >= 30
+//{
+//    soTien = soKM * 9500.0 + 11000.0 + (soKM - 30)*9000.0
+//    print("So tien phai tra la: \(soTien)")
+//}
+//MARK: BAI 3:
+//MARK: BAI 4: Sap xep phan tu
+//print("Chuong trinh sap xep phan tu")
+//print("Vui long nhap do dai mang")
+//var doDaiMang = Int(readLine() ?? "") ?? 0
+//var mangSo: [Int] = [doDaiMang]
+////Xuat ra cac gia tri tung phan tu cua mang
+//for i in 0...doDaiMang-1{
+//    print("mangInt[ \(i) ] = ")
+//    var insert = Int(readLine() ?? "Unknown") ?? 0
+//    mangSo.append(insert)
+//
+//}
+//var soLon: Int
+//var soBe: Int
+//for i in 0...doDaiMang-1{
+//    for j in i...doDaiMang-1
+//    {
+//        if mangSo[i] > mangSo[j]{
+//            var temp = mangSo[j]
+//            mangSo[j] = mangSo[i]
+//            mangSo[i] = temp
+//            print(mangSo[i])
+//        }
+//    }
+//}
+//print("Phan tu lon nhat la: \(mangSo.last)")
+//print("Phan tu nho nhat la: \(mangSo.first)")
+//MARK: 5
